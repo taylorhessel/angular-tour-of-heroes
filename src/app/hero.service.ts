6d6e6d5c75648c10199e0a33a7dd8993cbc31b7e
Taylor Hessel <taylorhessel@gmail.com>
2017-07-04 15:03:37 -0600

    Wrapped up Services portion of tutorial

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import Hero from './hero'
+import { HEROES } from './mock-heroes'
+
+@Injectable()
+export default class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES)
+  }
+
+  // Simulate a 2 second delay on promise resolution, imagine we're fetching from an actual API
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(this.getHeroes()), 2000)
+    })
+  }
+}
